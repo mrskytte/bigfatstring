4a09bd019ddd251244445842d89589214f4b1a88
Chris <mr.skytte@me.com>
2020-02-12 02:02:43 +0100

    Added a button that generates the output, rather than eventlistener on selector change

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 "use strict";
+window.addEventListener("DOMContentLoaded", init);
 // Define variable
 
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 const selector = document.querySelector("#selector");
+const submit = document.querySelector("#submit");
 
 // Read and store input data
 let currentInput;
@@ -11,10 +13,18 @@ let workingOutput;
 console.log(currentInput);
 
 // Read selected option
-selector.addEventListener("change", activateOption);
+
+function init() {
+  submit.addEventListener("click", generateOutput);
+}
+
+function generateOutput() {
+  activateOption();
+  writeOutput();
+}
 
 function activateOption() {
-  let thisOption = this.value;
+  let thisOption = selector.value;
   currentInput = input.value;
   workingOutput = currentInput;
   console.log(thisOption);
@@ -58,46 +68,46 @@ function firstName() {
   workingOutput =
     workingOutput[0].toUpperCase() +
     workingOutput.substring(1, workingOutput.length);
-  writeOutput();
 }
 
 // 2 Find the first name
 function fullNameOne() {
   workingOutput = workingOutput.substring(0, workingOutput.indexOf(" "));
-  writeOutput();
 }
 
 // 3 Find the length of the first name
 function fullNameTwo() {
   workingOutput = workingOutput.substring(0, workingOutput.indexOf(" "));
   workingOutput = workingOutput.length;
-  writeOutput();
 }
 
 // // 4 Find the middle name start and end position, and the middle name itself in a full name string
 function fullNameThree() {
-  workingOutput =
-    workingOutput.substring(
-      workingOutput.indexOf(" ") + 1,
-      workingOutput.lastIndexOf(" ")
-    ) +
-    ` : Starts at index ${workingOutput.indexOf(
-      " "
-    )} and ends at index ${workingOutput.lastIndexOf(" ")}`;
-  writeOutput();
+  workingOutput = workingOutput.substring(
+    workingOutput.indexOf(" ") + 1,
+    workingOutput.lastIndexOf(" ")
+  );
+  if (workingOutput[0] === " ") {
+    console.log("if");
+    workingOutput = "There is no middle name";
+  } else {
+    console.log(workingOutput.length);
+    workingOutput =
+      workingOutput +
+      ` : Starts at index ${workingOutput.indexOf(
+        " "
+      )} and ends at index ${workingOutput.lastIndexOf(" ")}`;
+  }
 }
 
 // // 5 Check if a filename is .png or .jp
 function filename() {
   if (workingOutput.endsWith(".png")) {
     workingOutput = "This is a .png-file";
-    writeOutput();
   } else if (workingOutput.endsWith(".jpg")) {
     workingOutput = "This is a .jpg-file";
-    writeOutput();
   } else {
     workingOutput = "This is not a .png-file or a .jpg-file";
-    writeOutput();
   }
 }
 
@@ -106,7 +116,6 @@ function password() {
   let passLen = workingOutput.length;
   workingOutput = "";
   workingOutput = workingOutput.padStart(passLen, "*");
-  writeOutput();
 }
 
 // // 7 Make the third character in a name uppercase
@@ -116,7 +125,6 @@ function anyOne() {
     workingOutput.substring(0, 2) +
     workingOutput[2].toUpperCase() +
     workingOutput.substring(3, workingOutput.length);
-  writeOutput();
 }
 
 // // 8 Make a character uppercase, if it follows a space or a hyphen
@@ -147,7 +155,6 @@ function anyTwo() {
       workingOutput.substring(0, number + 1) +
       workingOutput[number + 1].toUpperCase() +
       workingOutput.substring(number + 2, workingOutput.length);
-    writeOutput();
   }
 }
 
